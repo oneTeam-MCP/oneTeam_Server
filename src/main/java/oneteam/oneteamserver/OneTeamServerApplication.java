ce82e82ce7365331e1eaa6e84cd16cb93ef38925
shash0423 <shash042319@naver.com>
2025-08-07 21:37:36 +0900

    init: first commit

---
+++ src/main/java/oneteam/oneteamserver/OneTeamServerApplication.java
@@ -0,0 +1,13 @@
+package oneteam.oneteamserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneTeamServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OneTeamServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/oneteam/oneteamserver/OneTeamServerApplicationTests.java
@@ -0,0 +1,13 @@
+package oneteam.oneteamserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneTeamServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
