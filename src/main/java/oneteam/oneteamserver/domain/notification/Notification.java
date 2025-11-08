e1b36e19bc527c9b54e2845fa74e0b76d8f8a621
shash0423 <shash042319@naver.com>
2025-11-08 23:34:23 +0900

    fix: DB 컬럼명 일치

--- src/main/java/oneteam/oneteamserver/domain/notification/Notification.java
+++ src/main/java/oneteam/oneteamserver/domain/notification/Notification.java
@@ -6,7 +6,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.Immutable;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Entity
@@ -25,19 +24,11 @@ public class Notification {
 
     private String title;
 
-    private String url;
-
-    @Column(length = 50)
-    private String type;
-
-    @Column(length = 100)
-    private String author;
-
-    private LocalDate createdDate;
-
-    private Long views;
+    private String time_info;
 
     private String content;
+
+    private String link;
 
     private LocalDateTime crawledAt;
 
